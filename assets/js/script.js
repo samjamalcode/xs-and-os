41ea770d04e4e9fb3d58b7cca89d511287e683dd
samjamalcode <>
2023-11-12 09:02:45 +0000

    Start working in script.js file and add event Listener for 'DOMContentLoaded' to initialize the game when the page is loaded

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Add event Listener for 'DOMContentLoaded' to initialize the game when the page is loaded.
+document.addEventListener("DOMContentLoaded", () => {
+    let board = document.getElementById("board");
+    let status = document.getElementById("status");
+    let currentPlayer = "X";
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+    // Create cells and add click event listerner
+    
+})
